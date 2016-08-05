b8f47168aa26a14f51e5709a4d068ea6c02025fb
Tomster <tomster@emberjs.com>
2016-08-05 17:19:44 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/eecc9f99b58c02c2dc076a9abd816e53671933a0.

--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-alpha+7d0c6649
+ * @version   2.9.0-alpha+eecc9f99
  */
 
 var enifed, requireModule, require, Ember;
@@ -4326,9 +4326,12 @@ enifed('ember-application/system/engine-instance', ['exports', 'ember-runtime/sy
           return _this2.register(key, parent.resolveRegistration(key));
         });
 
-        ['router:main', _containerRegistry.privatize(_templateObject), '-view-registry:main'].forEach(function (key) {
+        ['router:main', _containerRegistry.privatize(_templateObject), '-view-registry:main', '-environment:main'].forEach(function (key) {
           return _this2.register(key, parent.lookup(key), { instantiate: false });
         });
+
+        this.inject('view', '_environment', '-environment:main');
+        this.inject('route', '_environment', '-environment:main');
       }
     });
   }
@@ -40344,7 +40347,7 @@ enifed('ember/index', ['exports', 'require', 'ember-metal', 'ember-runtime', 'em
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.9.0-alpha+7d0c6649";
+  exports.default = "2.9.0-alpha+eecc9f99";
 });
 enifed('glimmer-reference/index', ['exports', 'glimmer-reference/lib/reference', 'glimmer-reference/lib/const', 'glimmer-reference/lib/validators', 'glimmer-reference/lib/utils', 'glimmer-reference/lib/iterable'], function (exports, _glimmerReferenceLibReference, _glimmerReferenceLibConst, _glimmerReferenceLibValidators, _glimmerReferenceLibUtils, _glimmerReferenceLibIterable) {
   'use strict';
