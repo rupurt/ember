98020f08bdda379a72b017d55d6c09d6ee057b91
Tomster <tomster@emberjs.com>
2016-08-21 14:44:58 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/3ba7ae2b30bee93bf98023618ebe3d37b0206873.

--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-alpha+3f4ba8d4
+ * @version   2.9.0-alpha+3ba7ae2b
  */
 
 var enifed, requireModule, require, Ember;
@@ -10345,7 +10345,7 @@ enifed('ember-glimmer/syntax/curly-component', ['exports', 'glimmer-runtime', 'e
       i--;
     }
 
-    if (!seen['style']) {
+    if (seen.indexOf('style') === -1) {
       _emberGlimmerUtilsBindings.IsVisibleBinding.apply(component, operations);
     }
   }
@@ -40368,7 +40368,7 @@ enifed('ember/index', ['exports', 'require', 'ember-metal', 'ember-runtime', 'em
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.9.0-alpha+3f4ba8d4";
+  exports.default = "2.9.0-alpha+3ba7ae2b";
 });
 enifed('glimmer-reference/index', ['exports', 'glimmer-reference/lib/reference', 'glimmer-reference/lib/const', 'glimmer-reference/lib/validators', 'glimmer-reference/lib/utils', 'glimmer-reference/lib/iterable'], function (exports, _glimmerReferenceLibReference, _glimmerReferenceLibConst, _glimmerReferenceLibValidators, _glimmerReferenceLibUtils, _glimmerReferenceLibIterable) {
   'use strict';
