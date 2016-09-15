541fc744fc2611416ea838db2f66ac180a6fdcf4
Tomster <tomster@emberjs.com>
2016-09-15 19:53:02 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/51efb89068ffecd74ee5e269085a751e1f070172.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.10.0-alpha+e3cdc189
+ * @version   2.9.0-beta.2-alpha+51efb890
  */
 
 var enifed, requireModule, require, Ember;
@@ -4403,7 +4403,6 @@ enifed("ember-metal/debug", ["exports"], function (exports) {
   exports.deprecateFunc = deprecateFunc;
   exports.runInDebug = runInDebug;
   exports.debugSeal = debugSeal;
-  exports.debugFreeze = debugFreeze;
   var debugFunctions = {
     assert: function () {},
     info: function () {},
@@ -4418,8 +4417,7 @@ enifed("ember-metal/debug", ["exports"], function (exports) {
       return args[args.length - 1];
     },
     runInDebug: function () {},
-    debugSeal: function () {},
-    debugFreeze: function () {}
+    debugSeal: function () {}
   };
 
   exports.debugFunctions = debugFunctions;
@@ -4462,10 +4460,6 @@ enifed("ember-metal/debug", ["exports"], function (exports) {
 
   function debugSeal() {
     return debugFunctions.debugSeal.apply(undefined, arguments);
-  }
-
-  function debugFreeze() {
-    return debugFunctions.debugFreeze.apply(undefined, arguments);
   }
 });
 enifed('ember-metal/dependent_keys', ['exports', 'ember-metal/watching'], function (exports, _emberMetalWatching) {
@@ -5231,8 +5225,6 @@ enifed('ember-metal/index', ['exports', 'require', 'ember-metal/core', 'ember-me
   exports.runInDebug = _emberMetalDebug.runInDebug;
   exports.setDebugFunction = _emberMetalDebug.setDebugFunction;
   exports.getDebugFunction = _emberMetalDebug.getDebugFunction;
-  exports.debugSeal = _emberMetalDebug.debugSeal;
-  exports.debugFreeze = _emberMetalDebug.debugFreeze;
   exports.instrument = _emberMetalInstrumentation.instrument;
   exports.flaggedInstrument = _emberMetalInstrumentation.flaggedInstrument;
   exports._instrumentStart = _emberMetalInstrumentation._instrumentStart;
@@ -19127,7 +19119,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.10.0-alpha+e3cdc189";
+  exports.default = "2.9.0-beta.2-alpha+51efb890";
 });
 /*!
  * @overview RSVP - a tiny implementation of Promises/A+.
